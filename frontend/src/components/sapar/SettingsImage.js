fae20a1eac661c4f1fe18e7793b8182688c661ca
Bakish8 <omribakish8@gmail.com>
2022-01-11 19:37:39 +0200

    make it better

---
+++ frontend/src/components/sapar/SettingsImage.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Admin from '../shared/images/Admin.jpg'
+import { Parallax } from 'react-parallax'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const SettingsImage = () => (
+  <>
+    <h1 id='managerOptionsPageHeadLine' className='whiteme'>
+      הגדרות{' '}
+    </h1>
+    <Parallax className='imageParalaxAdmin' bgImage={Admin} strength={800}>
+      <div>
+        <Link style={{ textDecoration: 'none' }} to='/admin/settings/newtipul'>
+          <button id='centermeAndBlock' class='maketorbtn'>
+            <span>הוסף טיפול חדש</span>
+          </button>
+        </Link>
+
+        <Link style={{ textDecoration: 'none' }} to='/admin/reports/'>
+          <button id='centermeAndBlock' class='maketorbtn'>
+            <span>הגדר יום חופש</span>
+          </button>
+        </Link>
+        <Link style={{ textDecoration: 'none' }} to='/admin/userlist/'>
+          <button id='centermeAndBlock' class='maketorbtn'>
+            <span>הגדר ספר חדש</span>
+          </button>
+        </Link>
+      </div>
+    </Parallax>
+  </>
+)
+
+export default SettingsImage
