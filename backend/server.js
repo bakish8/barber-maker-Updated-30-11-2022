e383434b494a545b8500671eca7f798d91adc0d4
Bakish8 <omribakish8@gmail.com>
2022-03-03 19:17:26 +0200

    push no succssses getting phone numbers FROM google roll back...

--- backend/server.js
+++ backend/server.js
@@ -87,53 +87,8 @@ passport.use(
     async function (accessToken, refreshToken, profile, cb) {
       const googleuser = await User.findOne({ googleId: profile.id })
       console.log(`gogole user name is :${profile.name}`)
-      console.log(`gogole user is :${googleuser}`) /**renmove** */
       if (!googleuser) {
         console.log('no google user found! create')
-        /******************************************************************* */
-        console.log(`__________________________________`) /**renmove** */
-        var baseUrl = `https://people.googleapis.com/v1/people/${profile.id}?personFields=phoneNumbers` /***NEW */
-        console.log('baseUrl:' + baseUrl)
-        var getAADProfile = (callback) => {
-          var options = {
-            url: baseUrl,
-            headers: {
-              Authorization: 'Bearer ' + accessToken,
-            },
-          }
-          console.log('Requesting to ' + options.url)
-
-          request(options, (err, response, body) => {
-            if (err) {
-              console.log('Error when calling ' + options.url)
-              console.log(err)
-              callback(err, null)
-            } else {
-              const profile = JSON.parse(body)
-              console.log(profile)
-              callback(null, profile)
-            }
-          })
-        }
-
-        getAADProfile((err, profile) => {
-          if (err) {
-            callback(err)
-          } else {
-            user.user_metadata = user.user_metadata || {}
-            user.user_metadata.phoneNumber = profile.phoneNumbers[0].value
-            const Phone_Number = user.user_metadata.phoneNumber
-            console.log(Phone_Number)
-            console.log(Phone_Number)
-            console.log(Phone_Number)
-            console.log(Phone_Number)
-          }
-        })
-
-        /******************************************************************* */
-        console.log(
-          `_______________REGILAR USER NO PHONE INCLUDED___________________`
-        ) /**renmove** */
         const profile22 = JSON.stringify(profile) /**renmove** */
         console.log(`profile:${profile22}`) /**renmove** */
         console.log(`__________________________________`) /**renmove** */
