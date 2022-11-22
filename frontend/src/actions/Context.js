70a79f746a9bde2bf47c8f6ea7114c4d9ee9fd49
Bakish8 <omribakish8@gmail.com>
2022-11-22 20:33:08 +0200

    no message

--- frontend/src/actions/Context.js
+++ frontend/src/actions/Context.js
@@ -6,13 +6,14 @@ export const myContext = createContext({})
 
 export default function Context(props) {
   const [userObject, setuserObject] = useState()
-
+  const userGoogleLogin = useSelector((state) => state.userGoogleLogin)
+  const { userGoogleInfo, Gsuccess } = userGoogleLogin
   const dispatch = useDispatch()
   useEffect(() => {
     axios.get('/getgoogleuser', { withCredentials: true }).then((res) => {
       dispatch(Googlelogin(res.data.email))
     })
-  }, [dispatch])
+  }, [res])
 
   return (
     <myContext.Provider value={userObject}>{props.children}</myContext.Provider>
