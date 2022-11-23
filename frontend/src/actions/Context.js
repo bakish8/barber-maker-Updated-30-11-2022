8962ef4a4ec9cc58d4c2112f1c295ad08da32781
Bakish8 <omribakish8@gmail.com>
2022-11-23 09:14:35 +0200

    no message

--- frontend/src/actions/Context.js
+++ frontend/src/actions/Context.js
@@ -4,22 +4,17 @@ import { Googlelogin, googleuserResponse, login } from './userActions'
 import axios from 'axios'
 import { GOOGLE_USER_LOGIN_EMAIL_RESET } from '../constants/userConstants'
 export const myContext = createContext({})
-
+const googleuserResponses = useSelector((state) => state.googleuserResponses)
+const { RuserGoogleInfo, GRsuccess } = googleuserResponses
 export default function Context(props) {
   const [userObject, setuserObject] = useState()
-  const googleuserResponses = useSelector((state) => state.googleuserResponses)
-  const { RuserGoogleInfo, GRsuccess } = googleuserResponses
+
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(googleuserResponse)
     // axios.get('/getgoogleuser', { withCredentials: true }).then((res) => {
     //   dispatch(Googlelogin(res.data.email))
     if (GRsuccess && RuserGoogleInfo) {
-      console.log(RuserGoogleInfo)
-      console.log(RuserGoogleInfo.res)
-      console.log(RuserGoogleInfo)
-      console.log(RuserGoogleInfo).res.data
-      console.log(RuserGoogleInfo)
       console.log(RuserGoogleInfo)
 
       dispatch({
