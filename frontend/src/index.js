510f0fef17a9463e5a9aa2690a6f8ac7f39f2f02
Bakish8 <omribakish8@gmail.com>
2022-11-27 09:53:51 +0200

    register react app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux' //
+import { Provider } from 'react-redux'
 import store from './store' //פרוביידר סטור עוטף את האפליקציה שלנו כדי לאפשר שימוש ברדוקס
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import Context from './actions/Context' ///do somthing with google user to stop error ecrty page
+import Context from './actions/Context'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,4 +20,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
